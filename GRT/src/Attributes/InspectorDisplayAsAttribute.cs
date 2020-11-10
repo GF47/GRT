92c8b52182e9b652d407a2b3cb34157086a623ca
GF47 <liougourenmo@gmail.com>
2020-11-10 18:19:47 +0800

    add InspectorDisplayAsAttribute

---
+++ GRT/src/Attributes/InspectorDisplayAsAttribute.cs
@@ -0,0 +1,26 @@
+﻿/***************************************************************
+ *@File Name     : InspectorDisplayAsAttribute.cs
+ *@Author        : GF47
+ *@Description   : 字段自定义 Inspector 显示
+ *@Data          : 2020-11-10 15:22
+ *@Edit          : none
+ **************************************************************/
+
+namespace GRT
+{
+    using System;
+    using UnityEngine;
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class InspectorDisplayAsAttribute : PropertyAttribute
+    {
+        public InspectorDisplayAsAttribute(string name, string tips = "")
+        {
+            Name = name;
+            Tips = tips;
+        }
+
+        public string Name { get; }
+        public string Tips { get; }
+    }
+}
