84e92e2547af4b1349c47357b4d00b117b940ff3
GF47 <liougourenmo@gmail.com>
2020-11-19 17:35:18 +0800

    Add: TContainer <==> MonoCarrier

---
+++ GRT/src/TContainer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+
+namespace GRT
+{
+    public class TContainer<T> : MonoBehaviour
+    {
+        public T Content { get; set; }
+
+        public event Action OnAwake;
+        public event Action OnStart;
+        public event Action OnEnable_;
+        public event Action OnUpdate;
+        public event Action OnLateUpdate;
+        public event Action OnFixedUpdate;
+        public event Action OnDisable_;
+        public event Action OnDestroy_;
+
+        private void Awake() { OnAwake?.Invoke(); }
+        private void Start() { OnStart?.Invoke(); }
+        private void OnEnable() { OnEnable_?.Invoke(); }
+        private void Update() { OnUpdate?.Invoke(); }
+        private void LateUpdate() { OnLateUpdate?.Invoke(); }
+        private void FixedUpdate() { OnFixedUpdate?.Invoke(); }
+        private void OnDisable() { OnDisable_?.Invoke(); }
+        private void OnDestroy() { OnDestroy_?.Invoke(); }
+    }
+}
