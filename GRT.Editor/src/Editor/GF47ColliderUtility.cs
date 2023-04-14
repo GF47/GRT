e92d84f11b568636e74ffd6fba99e59c75ac344b
GF47 <liougourenmo@gmail.com>
2023-04-14 16:44:06 +0800

    add a panel to select gameobject from file, support regex

--- GRT.Editor/src/Editor/GF47ColliderUtility.cs
+++ GRT.Editor/src/Editor/GF47ColliderUtility.cs
@@ -10,92 +10,214 @@ namespace GRT.Editor
 {
     public class GF47ColliderUtility : ScriptableObject
     {
-        private static bool isActive;
+        private static bool _isActive;
 
         [MenuItem("Tools/GF47 Editor/Collider Utility")]
         private static void Init()
         {
-            if (isActive)
-            {
-                SceneView.duringSceneGui -= SceneView_duringSceneGui;
-            }
-            else
+            _isActive = !_isActive;
+            SceneView.duringSceneGui -= SceneView_duringSceneGui;
+            if (_isActive)
             {
                 SceneView.duringSceneGui += SceneView_duringSceneGui;
             }
-            isActive = !isActive;
         }
 
         private static void SceneView_duringSceneGui(SceneView view)
         {
             Handles.BeginGUI();
-
-            if (GUILayout.Button("X", GUILayout.Width(40f)))
+            GUILayout.BeginArea(new Rect(0f, 0f, 256f, 128f), EditorStyles.textArea);
             {
-                SceneView.duringSceneGui -= SceneView_duringSceneGui;
-                isActive = false;
-            }
-
-            if (GUILayout.Button("Create Collider", GUILayout.Width(200f)))
-            {
-                if (Selection.activeGameObject != null)
+                GUILayout.BeginHorizontal();
                 {
-                    var selectedObject = Selection.activeGameObject;
-
-                    var go = new GameObject(selectedObject.name);
-
-                    var meshes = selectedObject.GetComponentsInChildren<MeshFilter>();
-
-                    for (int i = 0; i < meshes.Length; i++)
+                    GUILayout.Label("Collider Utility", EditorStyles.boldLabel, GUILayout.Width(200f));
+                    if (GUILayout.Button("X", EditorStyles.miniButton, GUILayout.Width(48f)))
                     {
-                        var mesh = meshes[i];
-                        var collider = go.AddChild<BoxCollider>();
-                        collider.name = "collider " + i;
-                        collider.transform.localScale = mesh.transform.lossyScale;
-                        collider.transform.SetPositionAndRotation(mesh.transform.position, mesh.transform.rotation);
-                        collider.center = mesh.sharedMesh.bounds.center;
-                        collider.size = mesh.sharedMesh.bounds.size;
+                        _isActive = false;
+                        SceneView.duringSceneGui -= SceneView_duringSceneGui;
                     }
-
-                    Selection.activeGameObject = go;
                 }
-            }
+                GUILayout.EndHorizontal();
 
-            if (GUILayout.Button("Combine Colliders", GUILayout.Width(200f)))
-            {
-                var gameObjects = Selection.gameObjects;
-                
-                if (gameObjects != null)
+                GUILayout.Label("Create Collider To Wrap Selected");
+                GUILayout.BeginHorizontal();
                 {
-                    var o = new GameObject("_Combined Collider_");
-                    var c = o.AddComponent<BoxCollider>();
-
-                    var max = Vector3.negativeInfinity;
-                    var min = Vector3.positiveInfinity;
-
-                    for (int i = 0; i < gameObjects.Length; i++)
+                    if (GUILayout.Button("On Selected", GUILayout.Width(120f)))
                     {
-                        var colliders = gameObjects[i].GetComponentsInChildren<Collider>();
-
-                        if (colliders != null)
+                        if (Selection.activeGameObject != null)
                         {
-                            for (int j = 0; j < colliders.Length; j++)
-                            {
-                                // bounds.Encapsulate(colliders[j].bounds);
+                            var go = Selection.activeGameObject;
+                            Undo.RecordObject(go, "Add Collider");
 
-                                max = Vector3.Max(max, colliders[j].bounds.max);
-                                min = Vector3.Min(min, colliders[j].bounds.min);
+                            Mesh mesh;
+                            if (go.TryGetComponent<MeshFilter>(out var mf))
+                            {
+                                mesh = mf.sharedMesh;
+                            }
+                            else if (go.TryGetComponent<MeshCollider>(out var mc))
+                            {
+                                mesh = mc.sharedMesh;
+                            }
+                            else
+                            {
+                                throw new Exception($"{go.name} does not have a mesh");
                             }
 
+                            var collider = go.AddComponent<BoxCollider>();
+                            collider.center = mesh.bounds.center;
+                            collider.size = mesh.bounds.size;
+
+                            Undo.FlushUndoRecordObjects();
                         }
                     }
+                    if (GUILayout.Button("After Selected", GUILayout.Width(120f)))
+                    {
+                        if (Selection.activeGameObject != null)
+                        {
+                            var selected = Selection.activeGameObject.transform;
 
-                    c.center = (max + min) / 2f;
-                    c.size = max - min;
+                            var go = new GameObject($"{selected.name}_collider");
+                            Undo.RegisterCreatedObjectUndo(go, "Create Collider");
+                            Undo.RecordObject(go, "Add Collider");
 
-                    Selection.activeGameObject = o;
+                            Mesh mesh;
+                            if (selected.gameObject.TryGetComponent<MeshFilter>(out var mf))
+                            {
+                                mesh = mf.sharedMesh;
+                            }
+                            else if (selected.gameObject.TryGetComponent<MeshCollider>(out var mc))
+                            {
+                                mesh = mc.sharedMesh;
+                            }
+                            else
+                            {
+                                throw new Exception($"{go.name} does not have a mesh");
+                            }
+
+                            var collider = go.AddComponent<BoxCollider>();
+                            var transform = collider.transform;
+                            transform.SetParent(selected.parent);
+                            transform.SetSiblingIndex(selected.GetSiblingIndex() + 1);
+                            transform.localScale = selected.lossyScale;
+                            transform.SetPositionAndRotation(selected.position, selected.rotation);
+                            collider.center = mesh.bounds.center;
+                            collider.size = mesh.bounds.size;
+
+                            Undo.FlushUndoRecordObjects();
+                        }
+                    }
+                }
+                GUILayout.EndHorizontal();
+
+                GUILayout.Label("Create Colliders");
+                GUILayout.BeginHorizontal();
+                {
+                    if (GUILayout.Button("By MeshFilters", GUILayout.Width(120f)))
+                    {
+                        if (Selection.activeGameObject != null)
+                        {
+                            var selectedObject = Selection.activeGameObject;
+
+                            var go = new GameObject(selectedObject.name);
+                            Undo.RegisterCreatedObjectUndo(go, "Create Collider");
+                            Undo.RecordObject(go, "Add Collider");
+
+                            var meshes = selectedObject.GetComponentsInChildren<MeshFilter>();
+                            if (meshes != null && meshes.Length > 0)
+                            {
+                                for (int i = 0; i < meshes.Length; i++)
+                                {
+                                    var mesh = meshes[i];
+                                    var collider = go.AddChild<BoxCollider>();
+                                    collider.name = "collider " + i;
+                                    collider.transform.localScale = mesh.transform.lossyScale;
+                                    collider.transform.SetPositionAndRotation(mesh.transform.position, mesh.transform.rotation);
+                                    collider.center = mesh.sharedMesh.bounds.center;
+                                    collider.size = mesh.sharedMesh.bounds.size;
+                                }
+                            }
+
+                            Undo.FlushUndoRecordObjects();
+
+                            Selection.activeGameObject = go;
+                        }
+                    }
+                    if (GUILayout.Button("By MeshColliders", GUILayout.Width(120f)))
+                    {
+                        if (Selection.activeGameObject != null)
+                        {
+                            var selectedObject = Selection.activeGameObject;
+
+                            var go = new GameObject(selectedObject.name);
+                            Undo.RegisterCreatedObjectUndo(go, "Create Collider");
+                            Undo.RecordObject(go, "Add Collider");
+
+                            var meshColliders = selectedObject.GetComponentsInChildren<MeshCollider>();
+                            if (meshColliders != null && meshColliders.Length > 0)
+                            {
+                                for (int i = 0; i < meshColliders.Length; i++)
+                                {
+                                    var mc = meshColliders[i];
+                                    var collider = go.AddChild<BoxCollider>();
+                                    collider.name = "collider " + i;
+                                    collider.transform.localScale = mc.transform.lossyScale;
+                                    collider.transform.SetPositionAndRotation(mc.transform.position, mc.transform.rotation);
+                                    collider.center = mc.sharedMesh.bounds.center;
+                                    collider.size = mc.sharedMesh.bounds.size;
+                                }
+                            }
+
+                            Undo.FlushUndoRecordObjects();
+
+                            Selection.activeGameObject = go;
+                        }
+                    }
+                }
+                GUILayout.EndHorizontal();
+
+                if (GUILayout.Button("Combine", GUILayout.Width(240f)))
+                {
+                    var gameObjects = Selection.gameObjects;
+
+                    if (gameObjects != null)
+                    {
+                        var go = new GameObject("_Combined Collider_");
+
+                        Undo.RegisterCreatedObjectUndo(go, "Create Collider");
+                        Undo.RecordObject(go, "Add Collider");
+
+                        var c = go.AddComponent<BoxCollider>();
+
+                        var max = Vector3.negativeInfinity;
+                        var min = Vector3.positiveInfinity;
+
+                        for (int i = 0; i < gameObjects.Length; i++)
+                        {
+                            var colliders = gameObjects[i].GetComponentsInChildren<Collider>();
+
+                            if (colliders != null)
+                            {
+                                for (int j = 0; j < colliders.Length; j++)
+                                {
+                                    // bounds.Encapsulate(colliders[j].bounds);
+
+                                    max = Vector3.Max(max, colliders[j].bounds.max);
+                                    min = Vector3.Min(min, colliders[j].bounds.min);
+                                }
+
+                            }
+                        }
+
+                        c.center = (max + min) / 2f;
+                        c.size = max - min;
+
+                        Undo.FlushUndoRecordObjects();
+
+                        Selection.activeGameObject = go;
+                    }
                 }
             }
+            GUILayout.EndArea();
 
             Handles.EndGUI();
         }
---
+++ GRT.Editor/src/Editor/GF47SelectFromFile.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Windows;
+
+namespace GRT.Editor
+{
+    public class GF47SelectFromFile : EditorWindow
+    {
+        private static GUILayoutOption _height = GUILayout.Height(32f);
+        private string _pattern = @"(?<=entity\s+path\s*=\s*"")((.+):)?(.+?)(?="")";
+        private int _sceneGroup = 2;
+        private int _pathGroup = 3;
+        private string _text;
+
+        private IEnumerator<GameObject> _iterator;
+
+        [MenuItem("Tools/GF47 Editor/Select From File")]
+        private static void Init()
+        {
+            var window = GetWindow<GF47SelectFromFile>();
+            window.position = new Rect(300f, 300f, 320f, 200f);
+            window.minSize = new Vector2(320f, 200f);
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            _pattern = EditorGUILayout.DelayedTextField("Pattern", _pattern);
+            _sceneGroup = EditorGUILayout.DelayedIntField("Scene Group", _sceneGroup);
+            _pathGroup = EditorGUILayout.DelayedIntField("Path Group", _pathGroup);
+
+            var loaded = !string.IsNullOrEmpty(_text);
+
+            var defaultColor = GUI.color;
+            if (loaded) { GUI.color = Color.green; }
+            else { GUI.color = Color.yellow; }
+            if (GUILayout.Button(loaded ? "File Loaded" : "Load File", _height))
+            {
+                var path = EditorUtility.OpenFilePanel("Load File", Application.dataPath, "*");
+                Debug.Log(path);
+                if (File.Exists(path))
+                {
+                    _text = Encoding.UTF8.GetString(File.ReadAllBytes(path));
+                }
+            }
+
+            GUI.color = defaultColor;
+
+            if (loaded)
+            {
+                EditorGUILayout.BeginHorizontal();
+                {
+                    if (GUILayout.Button("Log Info", _height))
+                    {
+                        if (string.IsNullOrEmpty(_pattern))
+                        {
+                            Debug.Log(_text.Substring(0, 10000));
+                        }
+                        else
+                        {
+                            var sb = new StringBuilder();
+                            foreach (Match match in Regex.Matches(_text, _pattern))
+                            {
+                                var scene = _sceneGroup > -1 ? match.Groups[_sceneGroup].Value : match.Value;
+                                var path = _pathGroup > -1 ? match.Groups[_pathGroup].Value : match.Value;
+                                sb.AppendFormat("scene:\t{0}\tobject:\t{1}\n", scene, path);
+
+                                if (sb.Length >= 10000)
+                                {
+                                    sb.Append("...\netc.");
+                                    break;
+                                }
+                            }
+
+                            Debug.Log(sb);
+                        }
+                    }
+
+                    if (GUILayout.Button("All", _height))
+                    {
+                        var selected = FindGameObject();
+
+                        Selection.objects = selected.ToArray();
+                    }
+
+                    if (GUILayout.Button("Next", _height))
+                    {
+                        if (_iterator == null)
+                        {
+                            _iterator = FindGameObject().GetEnumerator();
+                        }
+
+                        if (_iterator.MoveNext())
+                        {
+                            Selection.activeGameObject = _iterator.Current;
+                        }
+                        else
+                        {
+                            _iterator.Dispose();
+                            _iterator = null;
+
+                            Debug.Log("Over");
+                        }
+                    }
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _iterator?.Dispose();
+        }
+
+        private List<GameObject> FindGameObject()
+        {
+            var selected = new List<GameObject>();
+
+            if (string.IsNullOrEmpty(_pattern))
+            {
+                var paths = _text.Split('\n');
+                foreach (var path in paths)
+                {
+                    var go = GameObject.Find(path);
+                    if (go != null)
+                    {
+                        selected.Add(go);
+                    }
+                }
+            }
+            else
+            {
+                foreach (Match match in Regex.Matches(_text, _pattern))
+                {
+                    var scene = _sceneGroup > -1 ? match.Groups[_sceneGroup].Value : match.Value;
+                    if (scene == SceneManager.GetActiveScene().name)
+                    {
+                        var path = _pathGroup > -1 ? match.Groups[_pathGroup].Value : match.Value;
+                        var go = GameObject.Find(path);
+                        if (go != null)
+                        {
+                            selected.Add(go);
+                        }
+                    }
+                }
+            }
+
+            return selected;
+        }
+    }
+}
