3587d2359b57e5779d039a7cf78b11ce6e4d7784
GF47 <liougourenmo@gmail.com>
2023-06-13 15:09:25 +0800

    add a approximately equal condition

---
+++ GRT/src/FSM/ApproximatelyEqualCondition.cs
@@ -0,0 +1,13 @@
+﻿namespace GRT.FSM
+{
+    public abstract class ApproximatelyEqualCondition<T> : ICondition
+    {
+        public abstract T Value { get; }
+
+        public abstract T Another { get; }
+
+        bool ICondition.OK => ApproximateEqual(Value, Another);
+
+        protected abstract bool ApproximateEqual(T a, T b);
+    }
+}
