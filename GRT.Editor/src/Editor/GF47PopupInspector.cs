5d83a67ef8a492f75db03570992bb85bb710c80f
GF47 <liougourenmo@gmail.com>
2026-07-20 09:00:00 +0800

    i popup inspector

---
+++ GRT.Editor/src/Editor/GF47PopupInspector.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace GRT.Editor
+{
+    public class GF47PopupInspector : ScriptableObject
+    {
+        [MenuItem("GameObject/GF47 Editor/Popup Inspector _i")]
+        private static void Popup()
+        {
+            var go = Selection.activeGameObject;
+            if (go != null)
+            {
+                var type = typeof(UnityEditor.Editor).Assembly.GetType("UnityEditor.InspectorWindow");
+                var window = EditorWindow.GetWindow(type);
+
+                window = EditorWindow.Instantiate(window);
+                window.minSize = new Vector2(400f, 300f);
+                window.position = new Rect(200f, 200f, 400f, 300f);
+                window.ShowUtility();
+
+                type.GetProperty("isLocked").SetValue(window, true);
+            }
+        }
+    }
+}
